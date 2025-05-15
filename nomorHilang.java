d1c51ce457418dc7574a1f145df755d2c0626479
Hello GudBayz <malangparanoid404@gmail.com>
2025-05-15 14:04:59 +0700

    Update nomorHilang.java

--- nomorHilang.java
+++ nomorHilang.java
@@ -13,13 +13,13 @@ public class Studycase {
 public static void main(String[] args) {
 
         int [] arr = {1,2,3,4,6,7,8};
-        System.out.println(nomorHilang(arr));  // Menambahkan pemanggilan fungsi dan print hasil
+        System.out.println(nomorHilang(arr));
     }
 
     public static int nomorHilang(int[] arr) {
-        int n = arr.length;  // sebelumnya salah penulisan: arr,length
+        int n = arr.length;
         int [] b = new int[arr[n-1] + 1];
-        for (int i = 0; i < n; i++) {  // sebelumnya: i=0l i<n (typo '0l' dan kurang tanda titik koma)
+        for (int i = 0; i < n; i++) {
             b[arr[i]] = 1;
         }
         for (int i = arr[0]; i <= arr[n-1]; i++) {
